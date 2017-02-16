0ad6598ede55dfbb19968ee84f33b95f114db3be
Roy Golan <rgolan@redhat.com>
2017-02-16 23:55:47 +0200

    Remove unused methods from Utils
    Change-Id: Ibdd3ca751740334c53001dbeb32581dfa04a9a7e
    Signed-off-by: Roy Golan <rgolan@redhat.com>

--- src/main/java/org/ovirt/vdsmfake/Utils.java
+++ src/main/java/org/ovirt/vdsmfake/Utils.java
@@ -15,11 +15,14 @@
  */
 package org.ovirt.vdsmfake;
 
-import java.io.PrintWriter;
-import java.io.StringWriter;
 import java.text.SimpleDateFormat;
-import java.util.*;
+import java.util.Date;
+import java.util.List;
+import java.util.Random;
+import java.util.TimeZone;
+import java.util.UUID;
 import java.util.concurrent.TimeUnit;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 /**
@@ -32,29 +35,20 @@ public class Utils {
     final static Random RND = new Random();
     final static String DATE_FORMAT = "yyy-MM-dd'T'HH:mm:ss z"; // 2013-02-10T19:09:11 GMT
     final static char[] MAC_VALS = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
-    private static boolean randomDelay=false;
+    private static boolean randomDelay;
     private static Long appConfigDelay;
-    private static int appConfigDelay_asInit=0;
+    private static int appConfigDelay_asInit;
     private static int minimum = 100;
 
     private static final Logger log = LoggerFactory.getLogger(Utils.class);
-
-
-    public static String getExceptionText(Throwable t) {
-        final StringWriter sw = new StringWriter();
-        final PrintWriter pw = new PrintWriter(sw);
-        new Exception().printStackTrace(pw);
-        return sw.getBuffer().toString();
-    }
 
     public static String getUuid() {
         return UUID.randomUUID().toString();
     }
 
-
     public static String getMacAddress() {
         // 80:C1:6E:6C:51:54
-        final StringBuilder b = new StringBuilder();
+        StringBuilder b = new StringBuilder();
         for (int i = 1; i <= 17; i++) {
             if (i % 3 == 0) {
                 b.append(":");
@@ -66,18 +60,10 @@ public class Utils {
         return b.toString();
     }
 
-    public String getNetworkBridgeName() {
-        return AppConfig.getInstance().getNetworkBridgeName();
-    }
-
     public static String getDateTimeGMT() {
-        final SimpleDateFormat sdf = new SimpleDateFormat(DATE_FORMAT);
+        SimpleDateFormat sdf = new SimpleDateFormat(DATE_FORMAT);
         sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
         return sdf.format(new Date());
-    }
-
-    public static String getRandIntegerAsString(int from, int to) {
-        return Integer.valueOf(from + RND.nextInt(to - from)).toString();
     }
 
     public static String getRandomNum(int length) {
@@ -174,49 +160,6 @@ public class Utils {
         return delay;
     }
 
-    public static int getInt(String val) {
-        if (val == null || val.trim().length() == 0) {
-            return 0;
-        }
 
-        try {
-            return Integer.valueOf(val);
-        } catch (Exception e) {
-            return 0;
-        }
-    }
 
-    public static long getLong(String val) {
-        if (val == null || val.trim().length() == 0) {
-            return 0;
-        }
-
-        try {
-            return Long.valueOf(val);
-        } catch (Exception e) {
-            return 0;
-        }
-    }
-
-    public static ArrayList<String> splitString(String s){
-        return new ArrayList(Arrays.asList(s.split(",")));
-    }
-
-    public static String ipv4toIpv6(String ip){
-        String[] octets = ip.split("\\.");
-        byte[] octetBytes = new byte[4];
-        for (int i = 0; i < 4; ++i) {
-            octetBytes[i] = (byte) Integer.parseInt(octets[i]);
-        }
-
-        byte ipv4asIpV6addr[] = new byte[16];
-        ipv4asIpV6addr[10] = (byte)0xff;
-        ipv4asIpV6addr[11] = (byte)0xff;
-        ipv4asIpV6addr[12] = octetBytes[0];
-        ipv4asIpV6addr[13] = octetBytes[1];
-        ipv4asIpV6addr[14] = octetBytes[2];
-        ipv4asIpV6addr[15] = octetBytes[3];
-
-        return Arrays.toString(ipv4asIpV6addr);
-    }
 }
