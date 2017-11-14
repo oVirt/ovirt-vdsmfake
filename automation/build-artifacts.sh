d7f4d0fba1b9b857465f1762d878ba464cc6240a
Roy Golan <rgolan@redhat.com>
2017-11-14 10:21:44 +0200

    Set maven version to 3.5.2 - fixes artifact building
    Change-Id: I546c01d2cbd386bc29540509e107a4bcee9f42da
    Signed-off-by: Roy Golan <rgolan@redhat.com>

--- automation/build-artifacts.sh
+++ automation/build-artifacts.sh
@@ -9,9 +9,10 @@ pushd $(dirname "$(readlink -f "$0")")/..
 
 mkdir -p ${ARTIFACT_DIR}
 
-# make sure we have maven 3.3 - wildfly-swarm must have it
-curl -sSL http://apache.spd.co.il/maven/maven-3/3.5.0/binaries/apache-maven-3.5.0-bin.tar.gz | tar -xzv
-export PATH=${PWD}/apache-maven-3.5.0/bin/:$PATH
+# make sure we have maven > 3.3 - wildfly-swarm must have it
+mvn_version=3.5.2
+curl -sSL http://apache.spd.co.il/maven/maven-3/$mvn_version/binaries/apache-maven-$mvn_version-bin.tar.gz | tar -xzv
+export PATH=${PWD}/apache-maven-$mvn_version/bin/:$PATH
 
 # bulid rpm
 ./build-rpm.sh --define "_rpmdir ${ARTIFACT_DIR}" --define "_release ${RELEASE}"
